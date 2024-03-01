5aaebe437a84a32c8e8e2159a455c96ba3e98312
pradhandebasish2023 <pradhandebasish2023@gmail.com>
2024-03-01 21:23:21 +0530

    result improved

--- output_script.py
+++ output_script.py
@@ -1,11 +1,11 @@
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 
 file_location = open('uploaded_file_location.txt', 'r').readline().strip()
 df = pd.read_csv(file_location)
 
-df_numeric = df.select_dtypes(include=['int64', 'float64'])
-sns.heatmap(df_numeric.corr(), annot=True, cmap='coolwarm')
-plt.title('Correlation Plot')
-plt.savefig('correlation_plot.png')
+plt.hist(df['SepalWidthCm'])
+plt.xlabel('Sepal Width (cm)')
+plt.ylabel('Frequency')
+plt.title('Distribution of Sepal Width')
+plt.savefig('sepal_width_distribution.png')
